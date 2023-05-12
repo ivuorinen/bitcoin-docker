f2c93d42214b5f8beb44e518acbb1b7cf3e8ab61
Kroese <kroese@users.noreply.github.com>
2023-05-12 17:45:16 +0200

    Print less debug info

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,7 @@ if [ -n "${GID+x}" ] && [ "${GID}" != "0" ]; then
   groupmod -g "$GID" bitcoin
 fi
 
-echo "$0: assuming uid:gid for bitcoin:bitcoin of $(id -u bitcoin):$(id -g bitcoin)"
-
 if [ $(echo "$1" | cut -c1) = "-" ]; then
-  echo "$0: assuming arguments for bitcoind"
-
   set -- bitcoind "$@"
 fi
 
@@ -24,9 +20,6 @@ if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "bitcoind" ]; then
   chown -R bitcoin:bitcoin "$(getent passwd bitcoin | cut -d: -f6)"
   # Fix permissions for bitcoin data dir.
   chown -R bitcoin:bitcoin "$BITCOIN_DATA"
-
-  echo "$0: setting data directory to $BITCOIN_DATA"
-
   set -- "$@" -datadir="$BITCOIN_DATA"
 fi
 
